79801d098403ac4e7906201679a0463464a6f519
FrontMaybeBackend <krzysztof231201@interia.pl>
2023-10-12 23:50:18 +0200

    refactor,make migrations with Phinx

--- database/migrations/001_create_table_currencies.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE currency_rate (
-                               id int(11) PRIMARY KEY AUTO_INCREMENT,
-                               table_name varchar(10) NOT NULL,
-                               effective_date date NOT NULL,
-                               currency varchar(100) NOT NULL,
-                               code varchar(10) NOT NULL,
-                               rate decimal(10,2) NOT NULL
-);
--- database/migrations/002_create_table_converted_currencies.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `converted_currencies` (
-                                        `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-                                        `initial_value` int(10) NOT NULL,
-                                        `source_currencies` varchar(10) NOT NULL,
-                                        `rate_kurs` varchar(10) NOT NULL,
-                                        `converted_amount` decimal(10,2) NOT NULL
-);
